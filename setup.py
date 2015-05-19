fb270dc74f1303dacd89d09b735b436d33cf798e
walkr <walkr@local>
2015-05-19 12:10:53 -0700

    Update install script

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 
-import os
-import platform
 import sys
-import warnings
 
 try:
     import setuptools
@@ -13,30 +10,46 @@ except ImportError:
     from distutils.core import setup
 
 
+is_py3 = sys.version_info.major == 3
 readme_file = 'README.md'
-try:
-    import pypandoc
-    long_description = pypandoc.convert(readme_file, 'rst')
-except (ImportError, OSError) as e:
-    print('No pypandoc or pandoc: %s' % (e,))
-    with open(readme_file) as fh:
-        long_description = fh.read()
 
-with open('./nanoservice/version.py') as fh:
-    for line in fh:
-        if line.startswith('VERSION'):
-            version = line.split('=')[1].strip().strip("'")
+
+def read_long_description(readme_file):
+    """ Read package long description from README file """
+    try:
+        import pypandoc
+    except (ImportError, OSError) as e:
+        print('No pypandoc or pandoc: %s' % (e,))
+        if is_py3:
+            fh = open(readme_file, encoding='utf-8')
+        else:
+            fh = open(readme_file)
+        long_description = fh.read()
+        fh.close()
+        return long_description
+    else:
+        return pypandoc.convert(readme_file, 'rst')
+
+
+def read_version():
+    """ Read package version """
+    with open('./nanoservice/version.py') as fh:
+        for line in fh:
+            if line.startswith('VERSION'):
+                return line.split('=')[1].strip().strip("'")
+
 
 setup(
     name='nanoservice',
-    version=version,
-    packages = ['nanoservice'],
+    version=read_version(),
+    packages=['nanoservice'],
     author='Tony Walker',
     author_email='walkr.walkr@gmail.com',
     url='https://github.com/walkr/nanoservice',
     license='MIT',
-    description='nanoservice is a small Python library for writing lightweight networked services using nanomsg',
-    long_description=long_description,
+    description='nanoservice is a small Python library for '
+                'writing lightweight networked services using nanomsg',
+    long_description=read_long_description(readme_file),
     install_requires=[
         'msgpack-python',
         'nanomsg',
@@ -54,5 +67,5 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
-        ],
+    ],
 )
