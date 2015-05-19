a62ce2c65572ae30450751255a5353936cce669d
walkr <walkr@local>
2015-05-19 13:55:34 -0700

    Fix pub-sub bugs

--- nanoservice/pubsub.py
+++ nanoservice/pubsub.py
@@ -8,11 +8,13 @@ from .error import PublisherError
 
 
 class Subscriber(Service):
-    """ Subscriber endpoint """
+    """ Subscriber """
 
-    def __init__(self, addr, encoder=None):
+    def __init__(self, addr, encoder=None,
+                 auth=False, secret=None, digestmod=None):
         socket = nanomsg.Socket(nanomsg.SUB)
-        super(Subscriber, self).__init__(addr, encoder, socket)
+        super(Subscriber, self).__init__(
+            addr, encoder, socket, auth, secret, digestmod)
 
     def get_fun_and_data(self, msg):
         for name in self.methods:
@@ -26,12 +28,21 @@ class Subscriber(Service):
     def register(self, name, fun):
         raise SubscriberError('Operation not allowed on this type of service')
 
-    def subscribe(self, name, fun):
-        super(Subscriber, self).register(name, fun)
-        self.sock.set_string_option(nanomsg.SUB, nanomsg.SUB_SUBSCRIBE, name)
+    def subscribe(self, tag, fun):
+        """ Subscribe and register a function """
+        super(Subscriber, self).register(tag, fun)
+        self.sock.set_string_option(nanomsg.SUB, nanomsg.SUB_SUBSCRIBE, tag)
+
+    def receive(self):
+        """ Receive request from client """
+        payload = self.sock.recv()
+        if self.authenticator:
+            self.authenticator.auth(payload)
+            payload = self.authenticator.unsigned(payload)
+        return payload
 
     def process(self):
-        msg = self.sock.recv()
+        msg = self.receive()
         fun, data = self.get_fun_and_data(msg)
 
         result = None
@@ -46,14 +57,22 @@ class Subscriber(Service):
 
 
 class Publisher(Client):
-    """ Publisher endpoint """
+    """ Publisher """
 
-    def __init__(self, addr, encoder=None):
+    def __init__(self, addr, encoder=None,
+                 auth=False, secret=None, digestmod=None):
         socket = nanomsg.Socket(nanomsg.PUB)
-        super(Publisher, self).__init__(addr, encoder, socket)
+        super(Publisher, self).__init__(
+            addr, encoder, socket, auth, secret, digestmod)
 
     def call(self, method, *args):
         raise PublisherError('Operation not allowed')
+
+    def send(self, payload):
+        """ Send payload through the socket """
+        if self.authenticator:
+            payload = self.authenticator.signed(payload)
+        self.sock.send(payload)
 
     def build_payload(self, tag, data):
         payload = bytes(tag.encode('utf-8')) + self.encode(data)
@@ -61,4 +80,4 @@ class Publisher(Client):
 
     def publish(self, tag, data):
         payload = self.build_payload(tag, data)
-        self.sock.send(payload)
+        self.send(payload)
