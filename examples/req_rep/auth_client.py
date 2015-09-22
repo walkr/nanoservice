64e27450426f94547356b24afb0aaa55c0e64f33
walkr <walkr@local>
2015-09-22 11:03:05 -0700

    Add new example service with authentication

---
+++ examples/req_rep/auth_client.py
@@ -0,0 +1,28 @@
+import time
+import hashlib
+
+from nanoservice import Client
+from nanoservice import Authenticator
+
+
+def main():
+    c = Client(
+        'ipc:///tmp/auth-example-service.sock',
+        authenticator=Authenticator('my-super-secret', hashlib.sha256)
+    )
+
+    n = 100
+    started = time.time()
+    for i in range(n):
+        _, err = c.call('generate_uuid')
+        assert err is None
+
+    duration = time.time() - started
+    print(
+        'Generated {} uuids in {:.3f} secs. ({:.2f} uuid/sec)'
+        .format(n, duration, n/duration)
+    )
+
+
+if __name__ == '__main__':
+    main()
---
+++ examples/req_rep/auth_service.py
@@ -0,0 +1,54 @@
+# Example service with authentication enabled
+
+# Start service with a configuration file:
+# > $ python auth_client.py auth_client.json
+
+import sys
+import time
+import uuid
+import random
+import hashlib
+import logging
+
+from nanoservice import Service
+from nanoservice import Authenticator
+from nanoservice import config
+
+
+CONF = None
+
+
+# *****************************************************
+# SERVICE METHODS
+# *****************************************************
+
+def generate_uuid():
+    """ Generate a random uuid """
+    return uuid.uuid4().hex
+
+
+# *****************************************************
+# MAIN
+# *****************************************************
+
+def main():
+    global CONF
+
+    time.sleep(random.random())
+    logging.basicConfig(level=logging.DEBUG)
+
+    conf_filepath = sys.argv[1]
+    CONF = config.load(conf_filepath)
+
+    service = Service(
+        'ipc:///tmp/auth-example-service.sock',
+        authenticator=Authenticator(
+            CONF.authenticator['secret'], hashlib.sha256
+        )
+    )
+
+    service.register('generate_uuid', generate_uuid)
+    service.start()
+
+if __name__ == '__main__':
+    main()
